5080a489440919a12f175782eac0f09ae06481b9
Pavana Paradesi <pp318@student.le.ac.uk>
2025-01-31 12:39:19 +0530

    code change

--- mern/backend/server.js
+++ mern/backend/server.js
@@ -8,14 +8,18 @@
 
 import express from "express";
 import cors from "cors";
-import records from "./routes/record.js";
+import recordRouter from "./routes/record.js";
+// import records from "./routes/record.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
+// const express = require("express");
+// const recordRouter = require("./routes/record");
 
+app.use("/api/employees", recordRouter);
 app.use(cors());
 app.use(express.json());
-app.use("/record", records);
+// app.use("/record", records);
 
 // start the Express server
 app.listen(PORT, () => {
